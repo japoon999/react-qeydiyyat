84aa62171e1c3dbb7c4c17fa7f155a38df919d37
japoon999 <aziz.azizli.95@inbox.ru>
2024-02-23 14:06:30 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    ad: "",
+    unvan: "",
+    telefon: "",
+    isDisabled: true,
+  };
 
-export default App;
+  change = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value }, this.checkFormValidity);
+  };
+
+  checkFormValidity = () => {
+    const { ad, unvan, telefon } = this.state;
+    if (ad !== "" && unvan !== "" && telefon !== "") {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  };
+
+  Submit = (e) => {
+    e.preventDefault();
+    alert("Qeydiyyatdan kecdiz");
+    this.setState({ ad: "", unvan: "", telefon: "", isDisabled: true });
+  };
+
+  render() {
+    const { isDisabled } = this.state;
+
+    return (
+      <div className="cont">
+        <form onSubmit={this.Submit}>
+          <label htmlFor="ad">
+            Ad, Soyad, Ata adi:{" "}
+            <input
+              type="text"
+              name="ad"
+              value={this.state.ad}
+              id="ad"
+              onChange={this.change}
+            />
+          </label>
+
+          <label htmlFor="unvan">
+            Unvan:{" "}
+            <input
+              type="text"
+              name="unvan"
+              value={this.state.unvan}
+              id="unvan"
+              onChange={this.change}
+            />
+          </label>
+
+          <label htmlFor="password">
+            Telefon:
+            <input
+              type="text"
+              name="telefon"
+              value={this.state.telefon}
+              id="telefon"
+              onChange={this.change}
+            />
+          </label>
+          <button type="submit" disabled={isDisabled}>Gonder</button>
+        </form>
+      </div>
+    );
+  }
+}
